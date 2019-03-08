07845aeae9b523b0389e42ee3829e722e5f507f6
松田 信介 <matsusin0413@gmail.com>
2019-03-09 03:14:48 +0900

    おめでとう表現追加！

--- lesson03/main.js
+++ lesson03/main.js
@@ -4,9 +4,14 @@ const SLOT1 = document.getElementById('slot1');
 const SLOT2 = document.getElementById('slot2');
 const SLOT3 = document.getElementById('slot3');
 const STARTBUTTON = document.getElementById('startButton');
+const MATCH = document.getElementById('congratulationBackground');
+const TEXT = document.getElementById('congratulationText');
+
+// MATCH.style.display = 'none';
 
 // 1/64で揃う
-var hoge = ["peach", "apple", "icecream", "sakura"];
+var hoge = ["peach", "apple"] // , "icecream", "sakura"];
+var slotVar = 2;
 var isPlay = true;
 
 function update() {
@@ -15,26 +20,40 @@ function update() {
     return;
   }
 
-  var randomNum = Math.floor((Math.random() * 4));
+  var randomNum = Math.floor((Math.random() * slotVar));
   SLOT1.src = `images/${hoge[randomNum]}.png`;
-  var randomNum = Math.floor((Math.random() * 4));
+  var randomNum = Math.floor((Math.random() * slotVar));
   SLOT2.src = `images/${hoge[randomNum]}.png`;
-  var randomNum = Math.floor((Math.random() * 4));
+  var randomNum = Math.floor((Math.random() * slotVar));
   SLOT3.src = `images/${hoge[randomNum]}.png`;
 }
 
 function stop() {
+  MATCH.style.display = 'block';
   isPlay = !isPlay;
 
   if (isPlay) {
     STARTBUTTON.innerText = "STOP";
+    MATCH.style.display = 'none';
   } else {
     STARTBUTTON.innerText = "PLAY";
   }
 
   if (SLOT1.src === SLOT2.src && SLOT2.src === SLOT3.src) {
-    alert("Congratulations!!");
+    MATCH.style.display = 'block';
+    TEXT.style.display = 'block';
+    congratulation();
   }
 }
+
+function congratulation() {
+  Particles.init({
+    selector: '.background',
+    sizeVariations: 30,
+    color: [
+      '#0bd', 'rgba(0,187,221,.5)', 'rgba(0,187,221,.2)'
+    ]
+  });
+};
 
 setInterval(update, intervalTime);
