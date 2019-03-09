681c6ba0fc03ba5d72650886e6d33a9b0c840514
松田信介 <matsusin0413@gmail.com>
2019-03-10 00:32:42 +0900

    基礎編拡充

---
+++ lesson00/TEST03/main.js
@@ -0,0 +1,2 @@
+alert(2 + 2);
+console.log(25 ** 5);
--- lesson02/main.js
+++ lesson02/main.js
@@ -1,25 +1,59 @@
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
-const intervalTime = 10;
+const intervalTime = 100;
 
-const DAYS = document.getElementById('days');
-const HOURS = document.getElementById('hours');
-const MINUTES = document.getElementById('minutes');
-const SECONDS = document.getElementById('seconds');
-const MILIS = document.getElementById('milliseconds');
+const SLOT1 = document.getElementById('slot1');
+const SLOT2 = document.getElementById('slot2');
+const SLOT3 = document.getElementById('slot3');
+const STARTBUTTON = document.getElementById('startButton');
+const MATCH = document.getElementById('congratulationBackground');
+const TEXT = document.getElementById('congratulationText');
 
-var countDown = new Date(Date.parse('2019-07-01'));
+// MATCH.style.display = 'none';
+
+// 1/64で揃う
+var hoge = ["peach", "apple"] // , "icecream", "sakura"];
+var slotVar = 2;
+var isPlay = true;
 
 function update() {
-  let now = new Date().getTime();
-  let distance = countDown - now;
-  DAYS.innerText = Math.floor(distance / (day));
-  HOURS.innerText = Math.floor((distance % (day)) / hour);
-  MINUTES.innerText = Math.floor((distance % (hour)) / minute);
-  SECONDS.innerText = Math.floor((distance % (minute)) / second);
-  MILIS.innerText = Math.floor((distance % (second)) / 10);
+
+  if (isPlay) {
+    return;
+  }
+
+  var randomNum = Math.floor((Math.random() * slotVar));
+  SLOT1.src = `images/${hoge[randomNum]}.png`;
+  var randomNum = Math.floor((Math.random() * slotVar));
+  SLOT2.src = `images/${hoge[randomNum]}.png`;
+  var randomNum = Math.floor((Math.random() * slotVar));
+  SLOT3.src = `images/${hoge[randomNum]}.png`;
 }
+
+function stop() {
+  MATCH.style.display = 'block';
+  isPlay = !isPlay;
+
+  if (isPlay) {
+    STARTBUTTON.innerText = "STOP";
+    MATCH.style.display = 'none';
+  } else {
+    STARTBUTTON.innerText = "PLAY";
+  }
+
+  if (SLOT1.src === SLOT2.src && SLOT2.src === SLOT3.src) {
+    MATCH.style.display = 'block';
+    TEXT.style.display = 'block';
+    congratulation();
+  }
+}
+
+function congratulation() {
+  Particles.init({
+    selector: '.background',
+    sizeVariations: 30,
+    color: [
+      '#0bd', 'rgba(0,187,221,.5)', 'rgba(0,187,221,.2)'
+    ]
+  });
+};
 
 setInterval(update, intervalTime);
