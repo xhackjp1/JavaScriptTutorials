c8ed066df5a5729a2c2e30f0cc01fbe3696e268e
松田信介 <matsusin0413@gmail.com>
2019-03-15 18:23:50 +0900

    修正データ追加

--- lesson02/main.js
+++ lesson02/main.js
@@ -3,15 +3,14 @@ const intervalTime = 100;
 const SLOT1 = document.getElementById('slot1');
 const SLOT2 = document.getElementById('slot2');
 const SLOT3 = document.getElementById('slot3');
+
 const STARTBUTTON = document.getElementById('startButton');
+
 const MATCH = document.getElementById('congratulationBackground');
 const TEXT = document.getElementById('congratulationText');
 
-// MATCH.style.display = 'none';
-
 // 1/64で揃う
-var hoge = ["peach", "apple"] // , "icecream", "sakura"];
-var slotVar = 2;
+var hoge = ["peach", "apple", "icecream", "sakura"];
 var isPlay = true;
 
 function update() {
@@ -20,11 +19,11 @@ function update() {
     return;
   }
 
-  var randomNum = Math.floor((Math.random() * slotVar));
+  var randomNum = Math.floor((Math.random() * hoge.length));
   SLOT1.src = `images/${hoge[randomNum]}.png`;
-  var randomNum = Math.floor((Math.random() * slotVar));
+  var randomNum = Math.floor((Math.random() * hoge.length));
   SLOT2.src = `images/${hoge[randomNum]}.png`;
-  var randomNum = Math.floor((Math.random() * slotVar));
+  var randomNum = Math.floor((Math.random() * hoge.length));
   SLOT3.src = `images/${hoge[randomNum]}.png`;
 }
 
--- vue/app.js
+++ vue/app.js
@@ -1,11 +1,82 @@
+const NYTBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
+const ApiKey = "fkR5bFRLhBfLpPhMofSyFxwdRavuCUet"; //
+
+function buildUrl (category) {
+  return NYTBaseUrl + category + ".json?api-key=" + ApiKey;
+}
+
+// const NYTBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
+// const ApiKey = "your_api_key_here"; // replace with NYT API key
+const SECTIONS = "home, arts, automobiles, books, business, fashion, food, health, insider, magazine, movies, national, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, tmagazine, travel, upshot, world";
+
+Vue.component('news-list', {
+  props: ['results'],
+  template: `
+    <section>
+      <div class="row" v-for="posts in processedPosts">
+        <div class="columns large-3 medium-6" v-for="post in posts">
+          <div class="card">
+          <div class="card-divider">
+          {{ post.title }}
+          </div>
+          <div class="" style="color: red;">
+          {{ post.byline }}
+          </div>
+          <a :href="post.url" target="_blank"><img :src="post.image_url"></a>
+          <div class="card-section">
+            <p>{{ post.abstract }}</p>
+          </div>
+        </div>
+        </div>
+      </div>
+  </section>
+  `,
+  computed: {
+    processedPosts() {
+      let posts = this.results;
+
+      // Add image_url attribute
+      posts.map(post => {
+        let imgObj = post.multimedia.find(media => media.format === "superJumbo");
+        post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
+      });
+
+      // Put Array into Chunks
+      let i, j, chunkedArray = [],
+        chunk = 4;
+      for (i = 0, j = 0; i < posts.length; i += chunk, j++) {
+        chunkedArray[j] = posts.slice(i, i + chunk);
+      }
+      return chunkedArray;
+    }
+  }
+});
+
 const vm = new Vue({
   el: '#app',
   data: {
-    results: [
-      {title: "the very first post", abstract: "lorem ipsum some test dimpsum"},
-      {title: "and then there was the second", abstract: "lorem ipsum some test dimsum"},
-      {title: "third time's a charm", abstract: "lorem ipsum some test dimsum"},
-      {title: "four the last time", abstract: "lorem ipsum some test dimsum"}
-    ]
+    results: [],
+    sections: SECTIONS.split(', '), // create an array of the sections
+    section: 'home', // set default section to 'home'
+    loading: true,
+    title: ''
+  },
+  mounted() {
+    this.getPosts('home');
+  },
+  methods: {
+    getPosts(section) {
+      let url = buildUrl(section);
+      console.log(url);
+
+      axios.get(url).then((response) => {
+        this.loading = false;
+        this.results = response.data.results;
+        let title = this.section !== 'home' ? "Top stories in '" + this.section + "' today" : "Top stories today";
+        this.title = title + "(" + response.data.num_results + ")";
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
   }
 });
